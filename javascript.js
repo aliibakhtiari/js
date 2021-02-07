436925a282f425b08edadcb4c025efb5eef94110
aliibakhtiari <77485073+aliibakhtiari@users.noreply.github.com>
2021-02-07 18:19:45 +0330

    init project

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var str = "The includes() method determines whether a string contains the characters of a specified string."
+var n = str.includes("contains")
+console.log(str)
+console.log(n)
+
+
+var strChar = String.fromCharCode(119,101,98,32,100,101,118,101,108,111,112,114);
+console.log(strChar)
