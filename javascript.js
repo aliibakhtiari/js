8ce33ac04b37b4d42c031e2be56d2ba1c949bef1
aliibakhtiari <77485073+aliibakhtiari@users.noreply.github.com>
2021-02-17 15:31:39 +0330

    project

--- javascript.js
+++ javascript.js
@@ -5,4 +5,34 @@ console.log(n)
 
 
 var strChar = String.fromCharCode(119,101,98,32,100,101,118,101,108,111,112,114);
-console.log(strChar)
+console.log(strChar);
+
+
+let txt = ""; 
+for(let count = 0; count < 10;count++) { 
+    console.log(txt+="*"); 
+}
+
+let spl = "salam chetori khoobi"
+let res = spl.split("")
+console.log(res);
+
+let reset = "salam chetori khoobi"
+let dec = reset.split("e")
+console.log(dec);
+
+let char = "saloom chetori khoobi"
+let charac = char.split("o",3)
+console.log(charac);
+
+let start = "Hello world, welcome to the universe."
+let strStart = start.startsWith("Hello");
+console.log(strStart);
+
+let star = "Hello world, welcome to the universe."
+let startStr = star.startsWith("welcome",13);
+console.log(startStr);
+
+var sub = "this is a test!";
+var strSub = sub.substr(5, 3);
+console.log(strSub)
